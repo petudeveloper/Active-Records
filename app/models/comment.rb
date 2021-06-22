ec234b7a453dcffa4ddac72e1d5c08103e8628f4
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-06-22 16:15:53 -0400

    Add validations for attributes in all class files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
+    validates :body, presence: true
+    validates :post_id, presence: true
     belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    validates :user_name, presence: true
+    validates :user_name, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
     has_many :posts
     has_many :comments, through: :posts
 end
