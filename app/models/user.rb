edb873758ea40194063c13e92452a4ee45e87e20
petumazo <davidalvarezmazzo@gmail.com>
2021-06-23 11:51:53 -0500

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :user_name, uniqueness: true
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :email, format: { with:URI::MailTo::EMAIL_REGEXP}
   has_many :posts
   has_many :comments, through: :posts
 end
